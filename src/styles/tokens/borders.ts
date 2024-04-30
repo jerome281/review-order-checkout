584a82bf0105251c0c4ad2cee1ae3ea85a805814
Jerome Degl'innocenti <jeromeinnocenti@gmail.com>
2024-04-30 14:37:13 +0100

    Set border color

--- src/styles/tokens/borders.ts
+++ src/styles/tokens/borders.ts
@@ -1,14 +1,14 @@
-import { colors } from './colors'
+import { colors } from './colors';
 
 const borders = {
-  default: `solid 2px ${colors.black}`,
-  valid: `solid 2px ${colors.black}`,
-  focus: `solid 2px ${colors.black}`,
-  disabled: `solid 2px ${colors.black}`,
-  error: `solid 2px ${colors.black}`
-} as const
+  default: `solid 1px ${colors.grey}`,
+  valid: `solid 1px ${colors.grey}`,
+  focus: `solid 1px ${colors.grey}`,
+  disabled: `solid 1px ${colors.grey}`,
+  error: `solid 1px ${colors.grey}`,
+} as const;
 
-export default borders
+export default borders;
 
-export type ThemeBorders = typeof borders
-export type ThemeBordersKeys = keyof typeof borders
+export type ThemeBorders = typeof borders;
+export type ThemeBordersKeys = keyof typeof borders;
