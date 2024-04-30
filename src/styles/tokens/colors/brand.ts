fe73705dd6cbe0b2c17d3ab97cdecf0cd433502d
Jerome Degl'innocenti <jeromeinnocenti@gmail.com>
2024-04-30 14:37:26 +0100

    Set brand colors

--- src/styles/tokens/colors/brand.ts
+++ src/styles/tokens/colors/brand.ts
@@ -1,4 +1,11 @@
 export const brandColors = {
-  black: '#000000',
+  black: '#2C3E50',
   white: '#FFFFFF',
+  grey: '#ECF0F1',
+  mediumGrey: '#B4BCC3',
+  lightGrey: '#F9F9F9',
+  orange: '#F39C12',
+  green: '#18BC9C',
+  red: '#E74C3C',
+  lightBlue: '#19BEC8',
 } as const;
